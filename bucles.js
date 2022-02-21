7c0c4efe9c0402fbff3f30653de60ac0437ebbc4
Sofia Linares <sofia.linares25@gmail.com>
2022-02-21 13:54:19 -0300

    tarea
    tarea de bucles y algoritmo JS

---
+++ bucles.js
@@ -0,0 +1,29 @@
+// Ejercicio contar del 1 al 10
+let i;
+
+for(i=1; i<=10;i=i+1){
+    console.log(i);
+}
+
+// Ejercicio 2
+
+let j;
+
+for(j=1;j<=100;j++){
+    if(j%5==0){
+        console.log(j);
+    }
+}
+
+// Ejercicio 3
+
+let n;
+let a;
+let m=0;
+
+for(a=1;a<=5;a++){
+    n = parseInt(prompt("ingrese el dato " +a+ "de 5"));
+    m = m+n; //acumulador 
+}
+console.log(`la suma total es: ${m}`);
+console.log (`El promedio es: ${m/5}`);
